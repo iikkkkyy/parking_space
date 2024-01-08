2ed50e1adb86a47c8929dd90e5f60c7a6765883f
kimkangtae <tt997768@gmail.com>
2024-01-08 17:59:30 +0900

    Feat: router, diSetup 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:parking_space/routes.dart';
+
+import 'di/di_setup.dart';
 
 void main() {
+  diSetup();
   runApp(const MyApp());
 }
 
@@ -10,14 +14,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }

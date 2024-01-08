5dcdc222e090974928d23410254618ad15d78eb8
kimkangtae <tt997768@gmail.com>
2024-01-08 16:45:11 +0900

    Feat: main_viewmodel 생성(debounce 적용)

---
+++ lib/ui/main_view_model.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:parking_space/data/repository/parking_lot_info_repository.dart';
+
+import 'main_state.dart';
+
+class MainVieModel extends ChangeNotifier {
+  final ParkingLotInfoRepository _repository;
+
+  MainState _state = const MainState();
+
+  MainState get state => _state;
+
+  final _debounce = Debounce(const Duration(milliseconds: 500));
+
+
+  MainVieModel({
+    required ParkingLotInfoRepository getParkingInfoRepository,
+  }) : _repository = getParkingInfoRepository;
+
+  Future<void> getParkingLotInfoList(String seoulRegionName) async {
+    _debounce.onEvent(() async {
+      _state = state.copyWith(isLoading: true);
+      notifyListeners();
+
+      final result = await _repository.getParkingLotInfoList(seoulRegionName);
+
+      _state = state.copyWith(parkingLotInfoList: result,
+        isLoading: false,);
+
+      notifyListeners();
+    });
+  }
+
+
+}
+
+class Debounce {
+  final Duration duration;
+  Timer? _timer;
+
+  Debounce(this.duration);
+
+  void onEvent(void Function() callback) {
+    if (_timer?.isActive ?? false) {
+      _timer?.cancel();
+    }
+    _timer = Timer(duration, () => callback());
+  }
+}

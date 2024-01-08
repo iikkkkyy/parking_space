c24f5e57ab467ae5ec517526c14bba7ac9499342
kimkangtae <tt997768@gmail.com>
2024-01-08 15:37:38 +0900

    Feat: api 연동 작성

---
+++ lib/data/data_source/parking_lot_info_api.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+class ParkingApi {
+  final String _baseUrl = 'http://openapi.seoul.go.kr:8088/sample/json/GetParkingInfo/1/5/';
+
+  final Dio _dio;
+
+  ParkingApi(this._dio);
+
+  Future<List<dynamic>> getParkingLotInfoList(String seoulRegionName) async {
+    final response = await _dio.get('$_baseUrl/$seoulRegionName');
+    final data = response.data as Map<String,dynamic>;
+    final realtimeParkingList = data['row'] as List<dynamic>;
+    return realtimeParkingList;
+  }
+}

6edc00fd630038b9166df92ad69fd7373772ff7d
kimkangtae <tt997768@gmail.com>
2024-01-08 16:11:36 +0900

    Refactor: constructor 변경

--- lib/data/data_source/parking_lot_info_api.dart
+++ lib/data/data_source/parking_lot_info_api.dart
@@ -1,15 +1,18 @@
 import 'package:dio/dio.dart';
 
 class ParkingApi {
-  final String _baseUrl = 'http://openapi.seoul.go.kr:8088/sample/json/GetParkingInfo/1/5/';
+  final String _baseUrl =
+      'http://openapi.seoul.go.kr:8088/sample/json/GetParkingInfo/1/5/';
 
   final Dio _dio;
 
-  ParkingApi(this._dio);
+  const ParkingApi({
+    required Dio dio,
+  }) : _dio = dio;
 
   Future<List<dynamic>> getParkingLotInfoList(String seoulRegionName) async {
     final response = await _dio.get('$_baseUrl/$seoulRegionName');
-    final data = response.data as Map<String,dynamic>;
+    final data = response.data as Map<String, dynamic>;
     final realtimeParkingList = data['row'] as List<dynamic>;
     return realtimeParkingList;
   }
